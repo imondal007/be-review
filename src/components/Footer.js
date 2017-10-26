a21abb02abbfbfdc12616635472280da0f8bce9d
Imran Mondal <hello@imranmondal.com>
2017-10-26 18:08:25 +0600

    Link updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return(
       <ul className="list-style-n">
-        <li>Organizing by <a href="https://www.facebook.com/groups/bebangladesh">Behance Banlgadesh 2017</a></li>
+        <li>Organized by <a href="https://www.facebook.com/bebangladesh/">Behance Banlgadesh</a></li>
         <li><a href="https://www.facebook.com/events/106525060024487">More On Facebook</a></li>
       </ul>
     )
