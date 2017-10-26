4c7f68d6088afdae4b1c7c8d2b63e01aba4aa3e2
Imran Mondal <hello@imranmondal.com>
2017-10-26 18:23:03 +0600

    Footer fb logo add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import './Footer.css'
 
+//import assets
+import fbLogo from '../img/facebook-logo.png'
+
 class Footer extends Component {
   render() {
     return(
       <ul className="list-style-n">
         <li>Organized by <a href="https://www.facebook.com/bebangladesh/">Behance Banlgadesh</a></li>
-        <li><a href="https://www.facebook.com/events/106525060024487">More On Facebook</a></li>
+        <li>More On <a href="https://www.facebook.com/events/106525060024487"> <img src={fbLogo} alt="Facebook"/> </a></li>
       </ul>
     )
   }
